22d58abe60a1e3ace5698fb4f063a28e41ce7b3a
Yasir Khan <71833669+yasir28991@users.noreply.github.com>
2022-12-24 12:40:49 +0500

    Worked on login-form

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let searchForm = document.querySelector('.search-form')
 let shoppingCart = document.querySelector('.shopping-cart')
+let loginForm = document.querySelector('.login-form')
 
 let searchBtn = document.getElementById('search-btn')
 let cartBtn = document.getElementById('cart-btn')
+let loginBtn = document.getElementById('login-btn')
 
 searchBtn.onclick = () => {
     searchForm.classList.toggle('active')
@@ -10,4 +12,8 @@ searchBtn.onclick = () => {
 
 cartBtn.onclick = () => {
     shoppingCart.classList.toggle('active')
+}
+
+loginBtn.onclick = () => {
+    loginForm.classList.toggle('active')
 }
