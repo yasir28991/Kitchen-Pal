ddc7d94bf3f471d743c5acae35ab9ea5d71aa37b
Yasir Khan <71833669+yasir28991@users.noreply.github.com>
2022-12-24 19:04:30 +0500

    completed header and media queries of it. (desktop)

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
 let searchForm = document.querySelector('.search-form')
 let shoppingCart = document.querySelector('.shopping-cart')
 let loginForm = document.querySelector('.login-form')
+let navBar = document.querySelector('.navbar')
 
 let searchBtn = document.getElementById('search-btn')
 let cartBtn = document.getElementById('cart-btn')
 let loginBtn = document.getElementById('login-btn')
+let menuBtn = document.getElementById('menu-btn')
 
 searchBtn.onclick = () => {
     searchForm.classList.toggle('active')
+    shoppingCart.classList.remove('active')
+    loginForm.classList.remove('active')
+    navBar.classList.remove('active')
 }
 
 cartBtn.onclick = () => {
     shoppingCart.classList.toggle('active')
+    searchForm.classList.remove('active')
+    loginForm.classList.remove('active')
+    navBar.classList.remove('active')
 }
 
 loginBtn.onclick = () => {
     loginForm.classList.toggle('active')
+    searchForm.classList.remove('active')
+    shoppingCart.classList.remove('active')
+    navBar.classList.remove('active')
 }
+
+menuBtn.onclick = () => {
+    navBar.classList.toggle('active')
+    searchForm.classList.remove('active')
+    shoppingCart.classList.remove('active')
+    loginForm.classList.remove('active')
+}
+
+window.addEventListener('scroll', () => {
+    searchForm.classList.remove('active')
+    shoppingCart.classList.remove('active')
+    loginForm.classList.remove('active')
+    navBar.classList.remove('active')
+})
