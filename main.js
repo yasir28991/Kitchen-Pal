ae0cb2f460841d586318862a3f6d4ef77fda262a
Yasir Khan <yasirkhan28991@gmail.com>
2022-12-26 17:47:44 +0000

    succesfully built product section with swiper js

--- main.js
+++ main.js
@@ -43,3 +43,25 @@ window.addEventListener("scroll", () => {
   loginForm.classList.remove("active");
   navBar.classList.remove("active");
 });
+
+
+var swiper = new Swiper(".product-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
