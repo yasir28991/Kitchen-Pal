07bbb4554fb28a03f64c3b2d51d0dd98f8e3168a
Yasir Khan <yasirkhan28991@gmail.com>
2022-12-30 20:09:22 +0000

    worked on search result item

---
+++ database/data.js
@@ -0,0 +1,32 @@
+export const products = [
+    {
+        name: 'Fresh Orange',
+        price: '1.99$ to 3.99$',
+        img: 'products/product-1.png',
+        rating: 4.5
+    },
+    {
+        name: 'Fresh Onions',
+        price: '1.99$ to 3.99$',
+        img: 'products/product-2.png',
+        rating: 2
+    },
+    {
+        name: 'Fresh meat',
+        price: '1.99$ to 3.99$',
+        img: 'products/product-3.png',
+        rating: 3
+    },
+    {
+        name: 'Fresh cabbage',
+        price: '1.99$ to 3.99$',
+        img: 'products/product-4.png',
+        rating: 3.5
+    },
+    {
+        name: 'Fresh fruits',
+        price: '1.99$ to 3.99$',
+        img: 'categories/cat-1.png',
+        rating: 4.5
+    }
+]
--- main.js
+++ main.js
@@ -1,14 +1,65 @@
+import { products } from "./database/data.js";
+
 let searchForm = document.querySelector(".search-form");
 let shoppingCart = document.querySelector(".shopping-cart");
 let loginForm = document.querySelector(".login-form");
 let navBar = document.querySelector(".navbar");
-const searchInput = document.getElementById('search-box')
-const clearBtn = document.getElementById('clear-search')
+const searchInput = document.getElementById("search-box");
+const clearBtn = document.getElementById("clear-search");
 
 let searchBtn = document.getElementById("search-btn");
 let cartBtn = document.getElementById("cart-btn");
 let loginBtn = document.getElementById("login-btn");
 let menuBtn = document.getElementById("menu-btn");
+
+// Determine rating stars
+const generateRatingStars = rating => {
+  let html = "";
+  const fullStars = Math.floor(rating);
+   // Number of full stars
+  const halfStars = rating % 1 !== 0 ? 1 : 0; 
+  // Number of half-stars (either 0 or 1)
+  const emptyStars = 5 - fullStars - halfStars;
+   // Number of empty stars
+  for (let i = 0; i < fullStars; i++) {
+    html += '<i class="fas fa-star"></i>'; 
+    // Add a full star
+  }
+  if (halfStars > 0) {
+    html += '<i class="fas fa-star-half"></i>'; 
+    // Add a half-star if needed
+  }
+  for (let i = 0; i < emptyStars; i++) {
+    html += '<i class="far fa-star"></i>'; 
+    // Add an empty star
+  }
+  return html;
+};
+
+
+// variables for products
+let productContainerOne = document.getElementById("product-container-one");
+// render the products
+function render() {
+  products.forEach(product => {
+    productContainerOne.innerHTML += `
+  <div class="swiper-slide box">
+    <img src="/Images/${product.img}" alt="p-1" />
+    <h3>${product.name}</h3>
+    <div class="price">${product.price}</div>
+    <div class="rating">
+      ${generateRatingStars(product.rating)}
+    </div>
+    <a href="#" class="btn">Add to cart</a>
+  </div>
+  `
+  })
+}
+
+render()
+
+
+// UI Code
 
 searchBtn.onclick = () => {
   searchForm.classList.toggle("active");
@@ -44,7 +95,6 @@ window.addEventListener("scroll", () => {
   loginForm.classList.remove("active");
   navBar.classList.remove("active");
 });
-
 
 var swiper = new Swiper(".product-slider", {
   loop: true,
@@ -89,12 +139,12 @@ var swiper = new Swiper(".review-slider", {
 });
 
 // clear search Button
-searchInput.addEventListener('input', () => {
-  clearBtn.style.display = searchInput.value ? 'block' : 'none'
-})
+searchInput.addEventListener("input", () => {
+  clearBtn.style.display = searchInput.value ? "block" : "none";
+});
 
-clearBtn.addEventListener('click', e => {
-  searchInput.value = ''
-  clearBtn.style.display = 'none'
-  e.preventDefault()
-})
+clearBtn.addEventListener("click", (e) => {
+  searchInput.value = "";
+  clearBtn.style.display = "none";
+  e.preventDefault();
+});
